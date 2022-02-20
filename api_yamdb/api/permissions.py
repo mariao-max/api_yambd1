bb0a40fa5c7b0ea094ba71d5b7821b44abf2e99e
Mamrenko-Alex <Mamrenkodev@yandex.com>
2022-02-20 09:49:37 +0100

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,20 +5,18 @@ class UserIsModerator(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        else:
-            return request.user.is_authenticated
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        else:
-            return (
-                request.user == obj.author
-                or (request.user.is_authenticated
-                    and request.user.is_moderator())
-                or (request.user.is_authenticated
-                    and request.user.is_admin())
-            )
+        return (
+            request.user == obj.author
+            or request.user.is_staff
+            or request.user.is_superuser
+            or request.user.is_moderator()
+            or request.user.is_admin()
+        )
 
 
 class UserIsAdmin(BasePermission):
