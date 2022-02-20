541e5d623db1240bc32b1f46407c573b5c788ea2
Mamrenko-Alex <Mamrenkodev@yandex.com>
2022-02-20 10:12:41 +0100

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+                                        IsAuthenticatedOrReadOnly, ReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Genre, Review, Title, User
@@ -100,6 +100,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
+    
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -114,6 +119,11 @@ class CommentViewSet(viewsets.ModelViewSet):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
 
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
+
 
 class CreateDestroyListViewSet(mixins.CreateModelMixin,
                                mixins.DestroyModelMixin,
@@ -125,19 +135,29 @@ class CreateDestroyListViewSet(mixins.CreateModelMixin,
 class CategoryViewSet(CreateDestroyListViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (UserIsAdminOrReadOnly, IsAuthenticatedOrReadOnly)
+    permission_classes = (UserIsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
 
 class GenreViewSet(CreateDestroyListViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (UserIsAdminOrReadOnly, IsAuthenticatedOrReadOnly)
+    permission_classes = (UserIsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -145,10 +165,15 @@ class TitleViewSet(viewsets.ModelViewSet):
         rating=Avg('reviews__score')
     ).all()
     serializer_class = TitleSerializer
-    permission_classes = (UserIsAdminOrReadOnly, IsAuthenticatedOrReadOnly)
+    permission_classes = (UserIsAdminOrReadOnly,)
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH',):
             return TitleCreateSerializer
         return TitleSerializer
+    
+    def get_permissions(self):
+        if self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
